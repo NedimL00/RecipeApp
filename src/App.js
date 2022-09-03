167f0689617f7cf2eca1a24d65e6e462ac5be83f
Nedim <lapo.nedim@gmail.com>
2022-09-03 13:44:47 +0200

    Removed Contact page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Footer from './components/layout/Footer';
 import Navbar from './components/layout/Navbar';
 import Home from './pages/Home';
 import About from './pages/About';
-import Contact from './pages/Contact';
 import NotFound from './pages/NotFound';
 import { RecipeProvider } from './context/recipe/RecipeContext';
 import { AlertProvider } from './context/alert/AlertContext';
@@ -27,7 +26,6 @@ function App() {
                   <Route path='/' element={<Home/>} />
                   <Route path='tutorial/:recipeID' element={<RecipeTutorial/>} />
                   <Route path='about' element={<About/>} />
-                  <Route path='contact' element={<Contact/>} />
                   <Route path='*' element={<NotFound />} />
                 </Routes>
               </main>
